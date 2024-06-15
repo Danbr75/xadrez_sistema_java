3f6af512287a63be6e26146616ec97e7277413d7
Danilo Brito <danbr7500@gmail.com>
2024-06-15 14:04:58 -0300

    iniciando implemento tabuleiro e peças

---
+++ src/boardGame/Piece.java
@@ -0,0 +1,23 @@
+package boardGame;
+
+import boardGame.Board;
+import boardGame.Position;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+
+}
+
+
